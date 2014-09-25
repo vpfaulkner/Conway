b88c829e9547ed8f2256902aa19e30460d1a774e
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-09-25 12:50:46 -0400

    Blank Wren project

---
+++ Conway/lib/Conway.rb
@@ -0,0 +1,5 @@
+class Conway
+  def run
+    fail "You need to add some functionality here before you can use this."
+  end
+end
---
+++ Conway/test/Conway_test.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require "Conway"
+
+class ConwayTest < MiniTest::Unit::TestCase
+  def test_first_test
+    assert false, "Please add a test here"
+  end
+end
